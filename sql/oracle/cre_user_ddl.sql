f58a0002254260cd03cedbfb2bcf06283d2f80f8
Mike Albert <mike.albert@cru.org>
2014-03-28 11:23:58 -0400

    Increase long and adjust headings for cre_user_ddl.

--- sql/oracle/cre_user_ddl.sql
+++ sql/oracle/cre_user_ddl.sql
@@ -1,8 +1,7 @@
 set verify off
-set heading off
-set long 9999
+set long 99999
 
-SELECT DBMS_METADATA.GET_DDL('USER',UPPER('&&user')) FROM dual;
-SELECT DBMS_METADATA.GET_GRANTED_DDL('ROLE_GRANT',UPPER('&&user')) from dual;
-SELECT DBMS_METADATA.GET_GRANTED_DDL('OBJECT_GRANT',UPPER('&&user')) from dual;
-SELECT DBMS_METADATA.GET_GRANTED_DDL('SYSTEM_GRANT',UPPER('&&user')) from dual;
+SELECT DBMS_METADATA.GET_DDL('USER',UPPER('&&user')) AS "Create User" FROM dual;
+SELECT DBMS_METADATA.GET_GRANTED_DDL('ROLE_GRANT',UPPER('&&user')) AS "Role Grants" from dual;
+SELECT DBMS_METADATA.GET_GRANTED_DDL('OBJECT_GRANT',UPPER('&&user')) AS "Object Grants" from dual;
+SELECT DBMS_METADATA.GET_GRANTED_DDL('SYSTEM_GRANT',UPPER('&&user')) AS "System Grants" from dual;
