0520bb7dc2c1d3905aa5674adbf36cc1ea79c10f
Mike Albert <mike.albert@cru.org>
2014-04-09 10:33:40 -0400

    Rename slow_24 scripts and allow user to specify number of days to analyze

--- sql/oracle/slow.sql
+++ sql/oracle/slow.sql
@@ -1,29 +1,31 @@
---most time consuming queries from the last 24 hours
+--slowest individual queries 
 
 set linesize 999
 set pagesize 50
+set verify off
 
 col module format a40
-col Schema format a10
+col Schema format a15
+
+accept numDays number prompt 'How many days to analyze? '
 
 SELECT * FROM (
 SELECT SQL_ID,
     PARSING_SCHEMA_NAME AS "Schema",
     MODULE,
     SUM(EXECUTIONS_TOTAL) AS "Total Executions",
-    ROUND((SUM(ELAPSED_TIME_TOTAL)/1000000)/SUM(EXECUTIONS_TOTAL),2) AS "Time per Execution (sec)",
-    ROUND((SUM(ELAPSED_TIME_TOTAL/1000000))/60,2) AS "Total Time (mins)"
+    ROUND((SUM(ELAPSED_TIME_TOTAL)/1000000)/SUM(EXECUTIONS_TOTAL),2) AS "Time per Execution (sec)"
   FROM DBA_HIST_SQLSTAT A,
     DBA_HIST_SNAPSHOT B
   WHERE A.SNAP_ID       = B.SNAP_ID
   AND A.DBID            = B.DBID
   AND A.INSTANCE_NUMBER = B.INSTANCE_NUMBER
-  AND B.BEGIN_INTERVAL_TIME BETWEEN TRUNC(SYSDATE)-1 AND TRUNC(SYSDATE)
+  AND B.BEGIN_INTERVAL_TIME BETWEEN TRUNC(SYSDATE)-&numDays AND TRUNC(SYSDATE)
   AND PARSING_SCHEMA_NAME NOT IN ('SYS','ORACLE_OCM','DBSNMP','MALBERT','SKOHLER','DAVIDH')
   AND EXECUTIONS_TOTAL <>0
   GROUP BY SQL_ID,
     PARSING_SCHEMA_NAME,
     MODULE
-  ORDER BY 6 DESC)
-  --show top 250 queries
-  WHERE ROWNUM < 251;
+  ORDER BY 5 DESC)
+  --only show queries longer than .5 seconds
+  WHERE "Time per Execution (sec)" > 0.5;
