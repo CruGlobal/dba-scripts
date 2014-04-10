5a9be25fcc19d63f1c4959058cc7749a92e15c12
Mike Albert <mike.albert@cru.org>
2014-04-10 16:49:18 -0400

    Add io_calibrate script

---
+++ sql/oracle/io_calibrate.sql
@@ -0,0 +1,12 @@
+SET SERVEROUTPUT ON
+DECLARE
+  lat  INTEGER;
+  iops INTEGER;
+  mbps INTEGER;
+BEGIN
+-- DBMS_RESOURCE_MANAGER.CALIBRATE_IO (<DISKS>, <MAX_LATENCY>, iops, mbps, lat);
+  DBMS_RESOURCE_MANAGER.CALIBRATE_IO (&disks, &latency, iops, mbps, lat);
+  DBMS_OUTPUT.PUT_LINE ('max_iops = ' || iops);
+  DBMS_OUTPUT.PUT_LINE ('latency  = ' || lat);
+  dbms_output.put_line('max_mbps = ' || mbps);
+end;
