c1d9b901d3c0e6402426d49d0c713d5855fe009c
Mike Albert <mike.albert@cru.org>
2014-05-21 11:43:34 -0400

    Modify workload stats script with fix for gathering stats

--- sql/oracle/workload_stats.sql
+++ sql/oracle/workload_stats.sql
@@ -1,9 +1,19 @@
--- view workload statistics
+-- procedure to gather stubborn workload stats
 -- must be run as sys
 
-col pval2 format a30
-
-set linesize 999
-set pagesize 999
-
-SELECT * FROM SYS.AUX_STATS$;
+col PNAME for a20
+   set pages 1000
+   select pname, pval1 from aux_stats$;
+   drop table testtab;
+   create table testtab(c1 number);
+   EXEC DBMS_STATS.GATHER_SYSTEM_STATS('START')
+   begin
+     for i in 1 .. 100000 loop
+       insert into testtab values(1);
+       commit;
+     end loop;
+   end;
+   /
+   select /*+ PARALLEL(testtab,2) */ * from testtab;
+   EXEC DBMS_STATS.GATHER_SYSTEM_STATS('STOP')
+   select pname, pval1 from aux_stats$;
