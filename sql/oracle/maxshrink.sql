e55b38d17e9d64fa878b5a100cc4c2f2414da897
Mike Albert <mike.albert@cru.org>
2014-05-28 11:27:57 -0400

    maxshrink script no longer trucnates filenames or commands

--- sql/oracle/maxshrink.sql
+++ sql/oracle/maxshrink.sql
@@ -1,50 +1,39 @@
-REM Filename      : maxshrink.sql
-REM Author        : Craig Richards - originally from Asktom.oracle.com
-REM Created       :
-REM Version       : 1.0
-REM Modifications :
-REM
-REM Description   : Displays the size your datafiles can be shrunk to, it also generates the commands for you
+set pages 0
+set lin 150
+set verify off
+column file_name format a80 word_wrapped
+column smallest format 999,990 heading "Smallest|Size|Poss."
+column currsize format 999,990 heading "Current|Size"
+column savings  format 999,990 heading "Poss.|Savings"
+column sum format 999,999,999
+break on report
+compute sum of savings on report
 
-SET VERIFY OFF
-
-COLUMN file_name FORMAT a50 word_wrapped
-COLUMN smallest FORMAT 999,990 HEADING "Smallest|Size|Poss."
-COLUMN currsize FORMAT 999,990 HEADING "Current|Size"
-COLUMN savings  FORMAT 999,990 HEADING "Poss.|Savings"
-
-BREAK ON report
-
-COMPUTE sum of savings on report
-
-COLUMN value new_val blksize
-
-SELECT value FROM v$parameter WHERE name = 'db_block_size'
+column value new_val blksize
+select value from v$parameter where name = 'db_block_size'
 /
 
-SELECT file_name,
-       CEIL( (NVL(hwm,1)*&&blksize)/1024/1024 ) smallest,
-       CEIL( blocks*&&blksize/1024/1024) currsize,
-       CEIL( blocks*&&blksize/1024/1024) -
-       CEIL( (NVL(hwm,1)*&&blksize)/1024/1024 ) savings
-FROM dba_data_files a,
-     ( SELECT file_id, max(block_id+blocks-1) hwm
-         FROM dba_extents
+select file_name,
+       ceil( (nvl(hwm,1)*&&blksize)/1024/1024 ) smallest,
+       ceil( blocks*&&blksize/1024/1024) currsize,
+       ceil( blocks*&&blksize/1024/1024) -
+       ceil( (nvl(hwm,1)*&&blksize)/1024/1024 ) savings
+from dba_data_files a,
+     ( select file_id, max(block_id+blocks-1) hwm
+         from dba_extents
         group by file_id ) b
-WHERE a.file_id = b.file_id(+)
+where a.file_id = b.file_id(+)
 /
 
-COLUMN cmd FORMAT a75 word_wrapped
+column cmd format a105 word_wrapped
 
-SELECT 'alter database datafile '''||file_name||''' resize ' ||
-       CEIL( (NVL(hwm,1)*&&blksize)/1024/1024 )  || 'm;' cmd
-FROM dba_data_files a,
-     ( SELECT file_id, max(block_id+blocks-1) hwm
-         FROM dba_extents
-        GROUP BY file_id ) b
-WHERE a.file_id = b.file_id(+)
-  AND CEIL( blocks*&&blksize/1024/1024) -
-      CEIL( (NVL(hwm,1)*&&blksize)/1024/1024 ) > 0
+select 'alter database datafile '''||file_name||''' resize ' ||
+       ceil( (nvl(hwm,1)*&&blksize)/1024/1024 )  || 'm;' cmd
+from dba_data_files a,
+     ( select file_id, max(block_id+blocks-1) hwm
+         from dba_extents
+        group by file_id ) b
+where a.file_id = b.file_id(+)
+  and ceil( blocks*&&blksize/1024/1024) -
+      ceil( (nvl(hwm,1)*&&blksize)/1024/1024 ) > 0
 /
-
-REM End of Script
