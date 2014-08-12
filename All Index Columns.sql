0d99ac3643e72b9bc3ae610a71c80780d0d15555
Phyllis Helton <phyllis.helton@cru.org>
2014-08-12 13:14:43 -0400

    Delete All Index Columns.sql

--- All Index Columns.sql
+++
@@ -1,8 +0,0 @@
-   SELECT table_owner,
-          table_name,
-          index_name,
-          LISTAGG(column_name, ', ') WITHIN GROUP (ORDER BY column_position) index_columns
-   FROM sys.dba_ind_columns
-   WHERE owner = :in_owner
-    AND  table_name = :in_table_name
-   GROUP BY table_owner, table_name, index_name;
