238da898dd7765bd752053987cafb0b566545dd3
Mike Albert <mike.albert@cru.org>
2014-09-17 14:44:39 -0400

    Just show the sql_text

--- sql/oracle/fulltext_awr.sql
+++ sql/oracle/fulltext_awr.sql
@@ -3,6 +3,6 @@
 set long 9999
 set pagesize 9999
 set verify off
-col sql_text format a60 
+col sql_text format a80 
 
-select sql_id, sql_text from dba_hist_sqltext where SQL_ID='&sql_id';
+select sql_text from dba_hist_sqltext where SQL_ID='&sql_id';
