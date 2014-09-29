c3cc80cf4ca5bb8a41a2c23063aba7993b4ff53f
Mike Albert <mike.albert@cru.org>
2014-09-29 14:47:45 -0400

    Print timestamp of sql execution along with binds

--- sql/oracle/find_bind.sql
+++ sql/oracle/find_bind.sql
@@ -8,6 +8,8 @@ prompt If SQL is older than 30 mins, use find_bind_awr.sql
 
 SELECT SQL_ID,
   NAME,
-  VALUE_STRING
+  VALUE_STRING,
+  LAST_CAPTURED
 FROM GV$SQL_BIND_CAPTURE
-WHERE SQL_ID='&sql_id';
+WHERE SQL_ID='&sql_id'
+ORDER BY LAST_CAPTURED DESC;
