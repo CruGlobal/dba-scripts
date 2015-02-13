00319a9a451c74bc185975a1b8658e9e5a6f490d
davidhollenberger <david.hollenberger@cru.org>
2015-02-13 15:01:44 -0500

    Get list of users with default passwords

---
+++ sql/oracle/default_pass.sql
@@ -0,0 +1,12 @@
+--------------------------------------------------------------------------------
+--
+-- File name:   default_pass.sql
+--
+-- Purpose:     Returns list of users that still have the default password 
+--
+-- Usage:      @default_pass.sql 
+--
+-- Other:       
+--
+--------------------------------------------------------------------------------
+select username, account_status from dba_users where username in (SELECT * FROM dba_users_with_defpwd);
