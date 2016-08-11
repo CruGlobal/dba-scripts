714f6d35d1744f6d1b17b2d9b7b76e772b2dab67
davidhollenberger <david.hollenberger@cru.org>
2016-08-11 10:20:14 -0400

    Check tablespace size on databases with 32k blk size

---
+++ sql/oracle/tablespace_size_32kblk.sql
@@ -0,0 +1,11 @@
+-- current tablespace size and free space
+
+set pagesize 50
+set feedback off
+
+SELECT tablespace_name               AS "Tablespace",
+  ROUND(used_space     *32768/1024/1024/1024,0) AS "Used(GB)",
+  ROUND(tablespace_size*32768/1024/1024/1024,0) AS "Size(GB)",
+  TRUNC(used_percent,2)                   AS "Used(%)"
+FROM dba_tablespace_usage_metrics
+ORDER BY used_percent DESC;
